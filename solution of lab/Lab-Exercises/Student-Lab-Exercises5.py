565434bec4338eb625df4b4f5261fdc6572dcdd2
Amir Hamza <amir.hamza0110@gmail.com>
2026-06-18 16:22:35 +0600

    solution of number 5

--- solution of lab/Lab-Exercises/Student-Lab-Exercises5.py
+++ solution of lab/Lab-Exercises/Student-Lab-Exercises5.py
@@ -1,14 +1,10 @@
-
 is_active = True
 cpu_percent = 94.5
 is_production = True
 
-should_alert = if cpu_percent > 90 and is_production = True or is_active = False ;
+should_alert = cpu_percent > 90 and is_production == True or is_active == False ;
 
 if should_alert:
     print("[ALERT] Urgent dispatch! System needs manual intervention.")
 else:
     print("[OK] System operating within safe margin bounds.")
-
-
-
